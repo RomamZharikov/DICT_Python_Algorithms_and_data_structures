b414203d80f84c0761070ac5b8ca33114668aa7e
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-04-03 14:11:55 +0300

    Completed test

--- CPU_info/tests.py
+++ CPU_info/tests.py
@@ -4,24 +4,29 @@ from generator import Generator
 
 def test_cpu_info():
     t = CPUInfo("AMD", "7 4700G", "AM4", [8, 8], [2.6, 3.64], 22)
-    assert t.manufacturer == "AMD"
-    assert t.model == "7 4700G"
-    assert t.socket == "AM4"
-    assert t.cores == [8, 8]
-    assert t.frequency == [2.6, 3.64]
-    assert t.tdp == 22
+    assert t.Manufacturer == "AMD"
+    assert t.Model == "7 4700G"
+    assert t.Socket == "AM4"
+    assert t.Cores == [8, 8]
+    assert t.Frequency == [2.6, 3.64]
+    assert t.TDP == 22
+
+
+def test_person_getinfo():
+    t = CPUInfo("AMD", "7 4700G", "AM4", [8, 8], [2.6, 3.64], 22)
+    assert isinstance(t.get_info(), str)
 
 
 def test_gen_single_types():
     g = Generator()
     g_one = g.generator()
     assert isinstance(g_one, CPUInfo)
-    assert isinstance(g_one.manufacturer, str)
-    assert isinstance(g_one.model, str)
-    assert isinstance(g_one.socket, str)
-    assert isinstance(g_one.cores, list)
-    assert isinstance(g_one.frequency, list)
-    assert isinstance(g_one.tdp, int)
+    assert isinstance(g_one.Manufacturer, str)
+    assert isinstance(g_one.Model, str)
+    assert isinstance(g_one.Socket, str)
+    assert isinstance(g_one.Cores, list)
+    assert isinstance(g_one.Frequency, list)
+    assert isinstance(g_one.TDP, int)
 
 
 def test_gen_1000_type():
